e4b913974ffc7d549915ead6ff4a34c98a844a94
Alexander Gates <al573409@dal.ca>
2023-02-05 22:30:34 -0400

    full week of all meals now available

--- src/pages/MealPlanner.js
+++ src/pages/MealPlanner.js
@@ -3,6 +3,8 @@ import Planner from '../components/planner';
 import NavBar from '../components/navbar';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
 
 const theme = createTheme();
 export default function MealPlanner() {
@@ -10,6 +12,13 @@ export default function MealPlanner() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <NavBar/>
+      
+      <ButtonGroup variant="contained" aria-label="outlined primary button group">
+        <Button>Last Week</Button>
+        <Button>Current Week</Button>
+        <Button>Next Week</Button>
+      </ButtonGroup>
+      
       <Planner/>
 
     </ThemeProvider>
