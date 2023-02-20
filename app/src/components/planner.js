78e42955ab0295a833c2da08ae05e999816fb81c
Alexander Gates <al573409@dal.ca>
2023-02-19 20:24:25 -0400

    directory restrucring & lambda code added

--- app/src/components/planner.js
+++ app/src/components/planner.js
@@ -10,6 +10,21 @@ export default function Planner() {
   const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
   return (
+    <Box sx={{ width: '98%', mx: 'auto', mt: '5%'}}>
+      <Grid container columnSpacing={{ xs: 1}}>
+        {days.map((day, index) => (
+          <Grid key={index} item xs={1} mr={0} sx={{ minWidth:210, border:1, borderRadius: '16px' }}>
+            <Typography>
+              {day}
+            </Typography>
+            <DayMeals/>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
+
+  /*return (
     <Box sx={{ width: '90%', mx: 'auto', mt: '8%'}}>
       <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 1, md: 1 }}>
         {days.map((day, index) => (
