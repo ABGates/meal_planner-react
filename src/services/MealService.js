ac6cdbb69a2c7da5b0f3d12f55d5756ce9ee9124
Alexander Gates <al573409@dal.ca>
2023-03-06 10:48:04 -0400

    updates to meal service

--- src/services/MealService.js
+++ src/services/MealService.js
@@ -21,3 +21,47 @@ export async function createMeal(meal) {
     }
 
 }
+
+export async function getAllMeals() {
+
+    try{
+        const response = await axios.get(
+            'https://7wszt61cwd.execute-api.us-east-1.amazonaws.com/default/get-all-meals', 
+            {
+                headers: {
+                    "access-control-allow-origin" : "*",
+                    "Content-type": "application/json; charset=UTF-8"
+                }
+            }
+        );
+        return response;
+    }
+    catch(error) {
+        console.log(error)
+        return null;
+    }
+
+}
+
+
+export async function getMeal(meal) {
+
+    try{
+        const response = await axios.post(
+            'https://7wszt61cwd.execute-api.us-east-1.amazonaws.com/default/get-meal',
+            meal, 
+            {
+                headers: {
+                    "access-control-allow-origin" : "*",
+                    "Content-type": "application/json; charset=UTF-8"
+                }
+            }
+        );
+        return response;
+    }
+    catch(error) {
+        console.log(error)
+        return null;
+    }
+
+}
