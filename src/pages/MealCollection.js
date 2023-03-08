652ee0d70d344a776bbf2d41ce52b81da0b5cd76
Alexander Gates <al573409@dal.ca>
2023-03-08 13:04:20 -0400

    new mapping

--- src/pages/MealCollection.js
+++ src/pages/MealCollection.js
@@ -17,6 +17,7 @@ export default function MealCollection() {
     getAllMeals().then(response => {
       console.log(response.data)
       console.log(typeof(response.data))
+      setMeals(response.data)
     })
 
     return (
