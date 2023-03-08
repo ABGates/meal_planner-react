2c9b77407d5b11a5bb4d0b7e2a9a1ebc35123402
Alexander Gates <al573409@dal.ca>
2023-03-08 12:53:08 -0400

    meal_collection testing

--- src/pages/MealCollection.js
+++ src/pages/MealCollection.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import NavBar from '../components/navbar';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
+import {useState} from "react";
+
+import NavBar from '../components/navbar';
 import MealCard from '../components/mealcard';
+import { getAllMeals } from '../services/MealService';
 
 const theme = createTheme();
 export default function MealCollection() {
+
+    const [meals, setMeals] = React.useState([1,2]); 
+    let temp = getAllMeals()
+    console.log(temp)
+
     return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
@@ -15,9 +23,11 @@ export default function MealCollection() {
 
       <Box sx={{ width: '90%', mx: 'auto', mt: '8%'}}>
       <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 1, md: 1 }}>
-          <Grid item xs={2} mr={10}>
+        {meals.map((index) => (
+          <Grid key={index} item xs={2} mr={10}>
             <MealCard/>
           </Grid>
+        ))}
       </Grid>
     </Box>
 
